61a5b009a2d14badd06cc1822ba96ec449c1c74b
chenyuanpei <chenyuanpei@chenyuanpeideMacBook-Pro.local>
2019-04-11 00:35:38 +0800

    wx.config

--- client/util/wxJs/call.js
+++ client/util/wxJs/call.js
@@ -28,7 +28,7 @@ const config = async() => {
   const signature = await getSignature()
   const wx = window.wx
   wx.config({
-    debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+    debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
     ...signature, // appId、nonceStr、timestamp、signature
     jsApiList: [ // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
       'checkJsApi',
@@ -79,7 +79,7 @@ const config = async() => {
     })
 
     wx.error((res) => {
-      console.error(res)
+      console.error('wx.config',res)
       // config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
       reject(res, wx)
     })
